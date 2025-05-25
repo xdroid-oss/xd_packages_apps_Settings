PixelOS-AOSP/packages_apps_Settings

Settings: Add high touch sensitivity and touchscreen hovering toggles
Author: Bruno Martins <bgcngm@gmail.com>
Date:   Tue Mar 26 00:06:11 2019 +0000

    DisplaySettings: Hide high touch sensitivity from search if needed

     * Fixes: https://gitlab.com/LineageOS/issues/android/issues/260

    Change-Id: I98ce66dec9801c36da5a2facd6b033895b7963f0

Author: Bruno Martins <bgcngm@gmail.com>
Date:   Tue Mar 26 00:20:37 2019 +0000

    LanguageAndInputSettings: Hide touch hovering from search if needed

    Change-Id: I5d53946262919ff67be5e777654af2171ffd2a44

Change-Id: Ib8712d2d0c0fe4396dbea7c7fc130e0c253d3207

src/com/android/settings/touch/HighTouchSensitivitySettingsPreferenceController.java
@@ -1,0 +1,46 @@
+/*
+ * Copyright (C) 2020 PixelExperience
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package com.android.settings.touch;
+
+import android.content.Context;
+import com.android.internal.custom.hardware.LineageHardwareManager;
+import com.android.settings.core.BasePreferenceController;
+
+public class HighTouchSensitivitySettingsPreferenceController extends BasePreferenceController {
+
+    public static final String KEY = "high_touch_sensitivity_enable";
+
+    private final LineageHardwareManager mHardware;
+
+    public HighTouchSensitivitySettingsPreferenceController(Context context, String preferenceKey) {
+        super(context, preferenceKey);
+
+        mHardware = LineageHardwareManager.getInstance(context);
+    }
+
+    public HighTouchSensitivitySettingsPreferenceController(Context context) {
+        this(context, KEY);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        if (!mHardware.isSupported(LineageHardwareManager.FEATURE_HIGH_TOUCH_SENSITIVITY)){
+            return UNSUPPORTED_ON_DEVICE;
+        }
+        return AVAILABLE;
+    }
+}

xdroid-oss/xd_packages_apps_Settings

src/com/android/settings/xdroid/touch/HighTouchSensitivitySettingsPreferenceController.java
@@ -1,0 +1,46 @@
+/*
+ * Copyright (C) 2020 PixelExperience
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package com.android.settings.xdroid.touch;
+
+import android.content.Context;
+import com.android.internal.lineage.hardware.LineageHardwareManager;
+import com.android.settings.core.BasePreferenceController;
+
+public class HighTouchSensitivitySettingsPreferenceController extends BasePreferenceController {
+
+    public static final String KEY = "high_touch_sensitivity_enable";
+
+    private final LineageHardwareManager mHardware;
+
+    public HighTouchSensitivitySettingsPreferenceController(Context context, String preferenceKey) {
+        super(context, preferenceKey);
+
+        mHardware = LineageHardwareManager.getInstance(context);
+    }
+
+    public HighTouchSensitivitySettingsPreferenceController(Context context) {
+        this(context, KEY);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        if (!mHardware.isSupported(LineageHardwareManager.FEATURE_HIGH_TOUCH_SENSITIVITY)){
+            return UNSUPPORTED_ON_DEVICE;
+        }
+        return AVAILABLE;
+    }
+}